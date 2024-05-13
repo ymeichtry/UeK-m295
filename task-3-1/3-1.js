f64ea2c827903a0eb301403ac629bc6a764a3aa3
Yanis Meichtry <yanis.meichtry@netcetera.com>
2024-05-13 14:35:43 +0000

    made task-3-1

---
+++ task-3-1/3-1.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/yanis', (request, response) => {
+    response.send('Hello yanis!');
+  });
+  
